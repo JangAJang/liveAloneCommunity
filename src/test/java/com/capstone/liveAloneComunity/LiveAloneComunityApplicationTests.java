6b178fa70b766883c79f1adf96e85580dc1faf44
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-12 13:50:53 +0900

    feat: initialize project

---
+++ src/test/java/com/capstone/liveAloneComunity/LiveAloneComunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.liveAloneComunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveAloneComunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
