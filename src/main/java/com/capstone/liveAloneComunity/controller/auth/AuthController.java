9b2465c15ec793a8a5aa97270f26dcea22e42942
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-18 21:27:22 +0900

    feat: AuthController 토큰을 재발행할 때, 해당 토큰을 바디로 반환해주는 컨트롤러 로직 구현

--- src/main/java/com/capstone/liveAloneComunity/controller/auth/AuthController.java
+++ src/main/java/com/capstone/liveAloneComunity/controller/auth/AuthController.java
@@ -1,6 +1,8 @@
 package com.capstone.liveAloneComunity.controller.auth;
 
+import com.capstone.liveAloneComunity.dto.auth.LogInRequestDto;
 import com.capstone.liveAloneComunity.dto.auth.RegisterRequestDto;
+import com.capstone.liveAloneComunity.dto.token.ReissueRequestDto;
 import com.capstone.liveAloneComunity.response.Response;
 import com.capstone.liveAloneComunity.service.AuthService;
 import io.swagger.annotations.ApiOperation;
@@ -22,5 +24,19 @@ public class AuthController {
     public Response register(@RequestBody @Valid RegisterRequestDto registerRequestDto){
         authService.register(registerRequestDto);
         return Response.success();
+    }
+
+    @PostMapping("/logIn")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "로그인", notes = "아이디와 비밀번호를 입력하면, 회원이 존재하고 비밀번호가 일치하면 토큰을 반환한다.")
+    public Response logIn(@RequestBody LogInRequestDto logInRequestDto){
+        return Response.success(authService.logIn(logInRequestDto));
+    }
+
+    @PatchMapping("/reissue")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "토큰 재발행", notes = "Access Token이 만료되었을 때(FrontEnd기준으론 쿠키가 만료되었을 떄) 토큰을 받아와 재발행해준다.")
+    public Response reissue(@RequestBody ReissueRequestDto reissueRequestDto){
+        return Response.success(authService.reissue(reissueRequestDto));
     }
 }
