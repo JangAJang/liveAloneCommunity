767612842b4f64904e419417d30cbf6363d46a8e
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-18 22:30:49 +0900

    feat: MemberController 회원 검색을 수행하는 동적쿼리로 조건에 맞는 회원을 검색하는 컨트롤러 로직 구현

--- src/main/java/com/capstone/liveAloneComunity/controller/member/MemberController.java
+++ src/main/java/com/capstone/liveAloneComunity/controller/member/MemberController.java
@@ -1,9 +1,12 @@
 package com.capstone.liveAloneComunity.controller.member;
 
+import com.capstone.liveAloneComunity.dto.member.SearchMemberDto;
 import com.capstone.liveAloneComunity.response.Response;
 import com.capstone.liveAloneComunity.service.member.MemberService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,5 +22,12 @@ public class MemberController {
     @ResponseStatus(HttpStatus.OK)
     public Response getMemberInfo(@RequestParam Long id){
         return Response.success(memberService.getMemberInfo(id));
+    }
+
+    @GetMapping("/search")
+    @ApiOperation(value = "회원 검색", notes = "회원의 DB의 번호, 아이디, 닉네임, 이메일을 검색한 값을 포함하는 페이지로 반환한다.")
+    @ResponseStatus(HttpStatus.OK)
+    public Response searchMember(@RequestBody SearchMemberDto searchMemberDto, @PageableDefault PageRequest pageRequest){
+        return Response.success(memberService.searchMember(searchMemberDto, pageRequest));
     }
 }
