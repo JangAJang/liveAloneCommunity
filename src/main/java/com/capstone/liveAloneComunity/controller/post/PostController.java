1a6eadbe8a0854fd77f09f240f7d5b5d8315bb94
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-19 18:23:23 +0900

    feat: PostController 전체 게시물에서 조건에 맞는 게시물을 검색하는 기능 구현

---
+++ src/main/java/com/capstone/liveAloneComunity/controller/post/PostController.java
@@ -0,0 +1,37 @@
+package com.capstone.liveAloneComunity.controller.post;
+
+import com.capstone.liveAloneComunity.dto.post.SearchPostRequestDto;
+import com.capstone.liveAloneComunity.repository.post.SearchPostType;
+import com.capstone.liveAloneComunity.response.Response;
+import com.capstone.liveAloneComunity.service.post.PostService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/post")
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/")
+    @ApiOperation(value = "게시물 단건 조회", notes = "게시물 단건을 조회한다.")
+    @ResponseStatus(HttpStatus.OK)
+    public Response getPost(@RequestParam("id") Long id){
+        return Response.success(postService.getPost(id));
+    }
+
+    @GetMapping("/search")
+    @ApiOperation(value = "게시물 검색", notes = "게시물을 검색해 페이징처리해 반환한다.")
+    @ResponseStatus(HttpStatus.OK)
+    public Response searchPost(@RequestBody SearchPostRequestDto searchPostRequestDto,
+                               @PageableDefault Pageable pageable,
+                               @RequestParam SearchPostType searchPostType){
+        return Response.success(postService.searchPost(searchPostRequestDto, pageable, searchPostType));
+    }
+}
