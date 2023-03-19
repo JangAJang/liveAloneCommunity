08438ed53420f21e50124c18c5abfd940f55b708
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-19 23:46:25 +0900

    feat: MemberServiceTest.EditTest 게시물을 수정할 때, 이메일 형식이 올바르지 않으면 예외처리시키는 것을 검증하는 테스트코드 구현

--- src/test/java/com/capstone/liveAloneComunity/service/member/EditTest.java
+++ src/test/java/com/capstone/liveAloneComunity/service/member/EditTest.java
@@ -3,6 +3,7 @@ package com.capstone.liveAloneComunity.service.member;
 import com.capstone.liveAloneComunity.dto.auth.RegisterRequestDto;
 import com.capstone.liveAloneComunity.dto.member.EditMemberInfoDto;
 import com.capstone.liveAloneComunity.entity.member.Member;
+import com.capstone.liveAloneComunity.exception.member.EmailNotFormatException;
 import com.capstone.liveAloneComunity.exception.member.MemberNotFoundException;
 import com.capstone.liveAloneComunity.repository.member.MemberRepository;
 import com.capstone.liveAloneComunity.service.auth.AuthService;
@@ -42,5 +43,26 @@ public class EditTest {
         //then
         Assertions.assertThat(member.getNickname()).isEqualTo("newNick");
         Assertions.assertThat(member.getEmail()).isEqualTo("new@e.com");
+    }
+
+    @Test
+    @DisplayName("닉네임과 이메일을 수정할 때, 이메일 형식이 올바르지 않으면 예외처리한다.")
+    public void editFail_EmailFormat() throws Exception{
+        //given
+        EditMemberInfoDto editMemberInfoDto = EditMemberInfoDto.builder()
+                .nickname("newNick")
+                .email("newe.com").build();
+        authService.register(RegisterRequestDto.builder()
+                .username("test")
+                .nickname("test")
+                .email("test@test.com")
+                .password("test")
+                .passwordCheck("test").build());
+        Member member = memberRepository.findByUsername_Username("test").orElseThrow(MemberNotFoundException::new);
+        //when
+
+        //then
+        Assertions.assertThatThrownBy(() -> memberService.editMember(member.getId(), editMemberInfoDto, member))
+                .isInstanceOf(EmailNotFormatException.class);
     }
 }
