a7d6056f4f99d39d2176f80a1e256b61677de85f
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-19 18:09:28 +0900

    feat: PostService 게시물을 수정하는 서비스 로직 구현'

--- src/main/java/com/capstone/liveAloneComunity/service/post/PostService.java
+++ src/main/java/com/capstone/liveAloneComunity/service/post/PostService.java
@@ -2,14 +2,13 @@ package com.capstone.liveAloneComunity.service.post;
 
 import com.capstone.liveAloneComunity.domain.post.Content;
 import com.capstone.liveAloneComunity.domain.post.Title;
-import com.capstone.liveAloneComunity.dto.post.MultiPostResponseDto;
-import com.capstone.liveAloneComunity.dto.post.PostResponseDto;
-import com.capstone.liveAloneComunity.dto.post.SearchPostRequestDto;
-import com.capstone.liveAloneComunity.dto.post.WritePostRequestDto;
+import com.capstone.liveAloneComunity.dto.post.*;
 import com.capstone.liveAloneComunity.entity.category.Category;
 import com.capstone.liveAloneComunity.entity.member.Member;
 import com.capstone.liveAloneComunity.entity.post.Post;
 import com.capstone.liveAloneComunity.exception.category.CategoryNotFoundException;
+import com.capstone.liveAloneComunity.exception.member.MemberNotAllowedException;
+import com.capstone.liveAloneComunity.exception.post.PostNotFoundException;
 import com.capstone.liveAloneComunity.repository.category.CategoryRepository;
 import com.capstone.liveAloneComunity.repository.post.PostRepository;
 import com.capstone.liveAloneComunity.repository.post.SearchPostType;
@@ -43,5 +42,22 @@ public class PostService {
         Page<PostResponseDto> searchResult = postRepository
                 .searchPost(searchPostRequestDto, searchPostType, pageable);
         return new MultiPostResponseDto(searchResult);
+    }
+
+    public MultiPostResponseDto getMembersPost(Pageable pageable, Long id){
+        Page<PostResponseDto> membersPost = postRepository.getMembersPost(id, pageable);
+        return new MultiPostResponseDto(membersPost);
+    }
+
+    public PostResponseDto editPost(EditPostRequestDto editPostRequestDto, Member currentMember, Long id){
+        Post post = postRepository.findById(id).orElseThrow(PostNotFoundException::new);
+        validatePostAuthority(currentMember, post);
+        post.editTitle(new Title(editPostRequestDto.getTitle()));
+        post.editContent(new Content(editPostRequestDto.getContent()));
+        return PostResponseDto.toDto(post);
+    }
+
+    public void validatePostAuthority(Member member, Post post){
+        if(!post.getMember().equals(member)) throw new MemberNotAllowedException();
     }
 }
