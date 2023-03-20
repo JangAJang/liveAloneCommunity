c006fb10dac5fee5c9c8f67eb00391af609613b9
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-20 12:43:27 +0900

    feat: PostServiceTest.writeTest 게시물 작성을 성공적으로 실행하는 것을 검증하는 테스트코드 구현'

--- src/main/java/com/capstone/liveAloneComunity/service/category/CategoryService.java
+++ src/main/java/com/capstone/liveAloneComunity/service/category/CategoryService.java
@@ -9,8 +9,6 @@ import com.capstone.liveAloneComunity.repository.category.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
---
+++ src/test/java/com/capstone/liveAloneComunity/service/post/PostServiceTest.java
@@ -0,0 +1,67 @@
+package com.capstone.liveAloneComunity.service.post;
+
+import com.capstone.liveAloneComunity.dto.auth.RegisterRequestDto;
+import com.capstone.liveAloneComunity.dto.post.WritePostRequestDto;
+import com.capstone.liveAloneComunity.entity.member.Member;
+import com.capstone.liveAloneComunity.exception.member.MemberNotFoundException;
+import com.capstone.liveAloneComunity.repository.member.MemberRepository;
+import com.capstone.liveAloneComunity.repository.post.PostRepository;
+import com.capstone.liveAloneComunity.service.auth.AuthService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Transactional
+public class PostServiceTest {
+
+    @Autowired
+    private AuthService authService;
+    @Autowired
+    private PostService postService;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private PostRepository postRepository;
+
+    @BeforeEach
+    void initTestData(){
+        IntStream.range(1, 11).forEach(i -> {
+            authService.register(RegisterRequestDto.builder()
+                    .username("test" + i)
+                    .nickname("test" + i)
+                    .email("test" + i + "@test.com")
+                    .password("test")
+                    .passwordCheck("test").build());
+            Member member = memberRepository.findByUsername_Username("test"+i).orElseThrow(MemberNotFoundException::new);
+            IntStream.range(i*10+1, i*10+6).forEach(index ->
+                    postService.writePost(member, WritePostRequestDto.builder()
+                            .title("title"+index)
+                            .content("content" + index).build()));
+        });
+    }
+
+    @Test
+    @DisplayName("")
+    public void createPostTest() throws Exception{
+        //given
+        authService.register(RegisterRequestDto.builder()
+                .username("test")
+                .nickname("test")
+                .email("test@test.com")
+                .password("test")
+                .passwordCheck("test").build());
+        Member member = memberRepository.findByUsername_Username("test").orElseThrow(MemberNotFoundException::new);
+        WritePostRequestDto writePostRequestDto = new WritePostRequestDto("title", "content");
+        //when
+        postService.writePost(member, writePostRequestDto);
+        //then
+        Assertions.assertThat(postRepository.findByTitle("title").isPresent()).isTrue();
+    }
+}
