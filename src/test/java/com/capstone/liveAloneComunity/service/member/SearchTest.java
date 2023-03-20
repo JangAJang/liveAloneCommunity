0957860c4a81edfa3306623e659c55ee99226b8b
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-20 12:33:25 +0900

    feat: MemberServiceTest.SearchTest 아이디를 기준으로 검색할 때, 회원을 검색해내는 것을 검증하는 테스트코드 구현

---
+++ src/test/java/com/capstone/liveAloneComunity/service/member/SearchTest.java
@@ -0,0 +1,59 @@
+package com.capstone.liveAloneComunity.service.member;
+
+import com.capstone.liveAloneComunity.dto.auth.RegisterRequestDto;
+import com.capstone.liveAloneComunity.dto.member.MemberResponseDto;
+import com.capstone.liveAloneComunity.dto.member.MemberSearchResultDto;
+import com.capstone.liveAloneComunity.dto.member.MemberSearchType;
+import com.capstone.liveAloneComunity.dto.member.SearchMemberDto;
+import com.capstone.liveAloneComunity.repository.member.MemberRepository;
+import com.capstone.liveAloneComunity.service.auth.AuthService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Transactional
+public class SearchTest {
+    @Autowired
+    private AuthService authService;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private MemberService memberService;
+
+    private static final PageRequest PAGE = PageRequest.of(0, 10);
+
+    @BeforeEach
+    void addTestData(){
+        memberRepository.deleteAll();
+        IntStream.range(1, 11).forEach(i ->
+                authService.register(RegisterRequestDto.builder()
+                        .username("test"+i)
+                        .nickname("test"+i)
+                        .email("test"+i+"@test.com")
+                        .password("test")
+                        .passwordCheck("test").build())
+        );
+    }
+
+    @Test
+    @DisplayName("아이디에 test를 검색하면 결과가 10개씩 페이징처리되어 반환된다.")
+    public void searchTest() throws Exception{
+        //given
+        SearchMemberDto searchMemberDto = new SearchMemberDto("test", MemberSearchType.USERNAME);
+        //when
+        MemberSearchResultDto memberSearchResultDto = memberService.searchMember(searchMemberDto, PAGE);
+        //then
+        Assertions.assertThat(memberSearchResultDto.getSearchResult().stream().map(MemberResponseDto::getUsername))
+                .containsExactly("test1", "test2", "test3", "test4", "test5",
+                        "test6", "test7", "test8", "test9", "test10");
+    }
+
+}
