87447541f88106fb1cc8a2fc299befee9f8bc517
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-20 12:53:54 +0900

    feat: PostServiceTest 게시물 단건을 조회할 때, 해당 게시물이 존재하면 그대로 ResponseDto로 반환하는 것을 검증하는 테스트코드 구현'

--- src/test/java/com/capstone/liveAloneComunity/service/post/PostServiceTest.java
+++ src/test/java/com/capstone/liveAloneComunity/service/post/PostServiceTest.java
@@ -2,10 +2,12 @@ package com.capstone.liveAloneComunity.service.post;
 
 import com.capstone.liveAloneComunity.dto.auth.RegisterRequestDto;
 import com.capstone.liveAloneComunity.dto.category.CategoryRequestDto;
+import com.capstone.liveAloneComunity.dto.post.PostResponseDto;
 import com.capstone.liveAloneComunity.dto.post.WritePostRequestDto;
 import com.capstone.liveAloneComunity.entity.category.Category;
 import com.capstone.liveAloneComunity.entity.member.Member;
 import com.capstone.liveAloneComunity.exception.member.MemberNotFoundException;
+import com.capstone.liveAloneComunity.exception.post.PostNotFoundException;
 import com.capstone.liveAloneComunity.repository.category.CategoryRepository;
 import com.capstone.liveAloneComunity.repository.member.MemberRepository;
 import com.capstone.liveAloneComunity.repository.post.PostRepository;
@@ -59,7 +61,7 @@ public class PostServiceTest {
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("카테고리 id, 제목, 내용을 Member 엔티티와 입력하면 게시물을 생성한다.")
     public void createPostTest() throws Exception{
         //given
         authService.register(RegisterRequestDto.builder()
@@ -75,5 +77,16 @@ public class PostServiceTest {
         postService.writePost(member, writePostRequestDto);
         //then
         Assertions.assertThat(postRepository.findByTitle_Title("title").isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("게시물의 아이디를 입력받으면 게시물 단건을 조회한다.")
+    public void getPostTest() throws Exception{
+        //given
+        Long id = postRepository.findByTitle_Title("title11").orElseThrow(PostNotFoundException::new).getId();
+        //when
+        PostResponseDto postResponseDto = postService.getPost(id);
+        //then
+        Assertions.assertThat(postResponseDto.getTitle()).isEqualTo("title11");
     }
 }
