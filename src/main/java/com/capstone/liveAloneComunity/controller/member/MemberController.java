3e5950622043ecf77e3c66e1701933e50480f66c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-25 22:21:54 +0900

    fix: MemberController uri통일

--- src/main/java/com/capstone/liveAloneComunity/controller/member/MemberController.java
+++ src/main/java/com/capstone/liveAloneComunity/controller/member/MemberController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/member")
+@RequestMapping("/api/members")
 public class MemberController {
 
     private final MemberService memberService;
