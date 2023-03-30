1d1223d6296ba04cbb694cd3b9fcc7b7e1c60677
eom-tae-in <eti0728@daum.net>
2023-03-30 17:48:41 +0900

    [feat] comment 도메인 생성

---
+++ src/main/java/com/capstone/liveAloneCommunity/domain/comment/Content.java
@@ -0,0 +1,11 @@
+package com.capstone.liveAloneCommunity.domain.comment;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Content {
+
+    String content;
+}
---
+++ src/main/java/com/capstone/liveAloneCommunity/entity/comment/Comment.java
@@ -0,0 +1,2 @@
+package com.capstone.liveAloneCommunity.entity.comment;public class Comment {
+}
