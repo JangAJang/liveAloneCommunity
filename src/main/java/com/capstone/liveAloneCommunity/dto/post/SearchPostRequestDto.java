fc7cb10ed3521a44f9fb72eaadedad23c6e67508
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-31 00:09:08 +0900

    [refactor] PostRepositoryCustomImpl 페이징처리하는 로직에서 존재할 모든 입력변수를 하나의 dto로 받아 내부에서 Pageable을 생성해 처리하도록 해당 기능을 사용하는 모든 로직에서 입력변수 수정

--- src/main/java/com/capstone/liveAloneCommunity/dto/post/SearchPostRequestDto.java
+++ src/main/java/com/capstone/liveAloneCommunity/dto/post/SearchPostRequestDto.java
@@ -5,22 +5,20 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Getter
 public class SearchPostRequestDto {
 
+    private int page;
+    private int size;
     @NotNull(message = "검색할 내용을 입력해주세요.")
     @NotBlank(message = "검색할 내용을 입력해주세요.")
     @NotEmpty(message = "검색할 내용을 입력해주세요.")
     private String text;
-
     private SearchPostType searchPostType;
-
-    public SearchPostRequestDto(String text, SearchPostType searchPostType) {
-        this.text = text;
-        this.searchPostType  = searchPostType;
-    }
 }
--- src/main/java/com/capstone/liveAloneCommunity/repository/post/PostRepositoryCustom.java
+++ src/main/java/com/capstone/liveAloneCommunity/repository/post/PostRepositoryCustom.java
@@ -1,5 +1,6 @@
 package com.capstone.liveAloneCommunity.repository.post;
 
+import com.capstone.liveAloneCommunity.dto.post.MembersPostRequestDto;
 import com.capstone.liveAloneCommunity.dto.post.PostResponseDto;
 import com.capstone.liveAloneCommunity.dto.post.SearchPostRequestDto;
 import org.springframework.data.domain.Page;
@@ -7,7 +8,7 @@ import org.springframework.data.domain.Pageable;
 
 public interface PostRepositoryCustom {
 
-    Page<PostResponseDto> searchPost(SearchPostRequestDto searchPostRequestDto, Pageable pageable);
+    Page<PostResponseDto> searchPost(SearchPostRequestDto searchPostRequestDto);
 
-    Page<PostResponseDto> getMembersPost(Long id, Pageable pageable);
+    Page<PostResponseDto> getMembersPost(MembersPostRequestDto membersPostRequestDto);
 }
