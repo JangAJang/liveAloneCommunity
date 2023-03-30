b533d0921b372744ba5979be794e443be87c3824
eom-tae-in <eti0728@daum.net>
2023-03-30 17:49:11 +0900

    [feat] comment 엔티티 생성

--- src/main/java/com/capstone/liveAloneCommunity/entity/comment/Comment.java
+++ src/main/java/com/capstone/liveAloneCommunity/entity/comment/Comment.java
@@ -1,2 +1,22 @@
-package com.capstone.liveAloneCommunity.entity.comment;public class Comment {
+package com.capstone.liveAloneCommunity.entity.comment;
+
+import com.capstone.liveAloneCommunity.entity.post.Post;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POST_ID")
+    private Post post;
 }
