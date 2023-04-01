7b16faf72fc11e34c1585e7fad24cc45095eae84
eom-tae-in <eti0728@daum.net>
2023-04-01 22:48:44 +0900

    [feat] CommentResponseDto 빌더 패턴 구현

--- src/main/java/com/capstone/liveAloneCommunity/dto/comment/CommentResponseDto.java
+++ src/main/java/com/capstone/liveAloneCommunity/dto/comment/CommentResponseDto.java
@@ -11,4 +11,12 @@ public class CommentResponseDto {
     private String content;
     private String nickname;
     private LocalDateTime createTime;
+
+    @Builder
+    public CommentResponseDto(Long id, String content, String nickname, LocalDateTime createTime) {
+        this.id = id;
+        this.content = content;
+        this.nickname = nickname;
+        this.createTime = createTime;
+    }
 }
