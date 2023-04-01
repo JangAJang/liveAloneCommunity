7de06703dd19c82a9934320d3ae9475d2514756d
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-01 18:31:00 +0900

    [refactor] 충돌이 발생하는 .gradle패키지 내부의 파일을 제거한 채로 최종 커밋

--- src/main/java/com/capstone/liveAloneCommunity/dto/member/SearchMemberDto.java
+++ src/main/java/com/capstone/liveAloneCommunity/dto/member/SearchMemberDto.java
@@ -3,14 +3,12 @@ package com.capstone.liveAloneCommunity.dto.member;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class SearchMemberDto {
 
     private int page;
