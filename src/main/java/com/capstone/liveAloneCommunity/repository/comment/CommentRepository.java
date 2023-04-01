6b5ebe77e8890f6c602d0e044c448a27d10932e4
eom-tae-in <eti0728@daum.net>
2023-04-01 21:31:42 +0900

    [feat] comment 서비스 생성

--- src/main/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepository.java
+++ src/main/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepository.java
@@ -1,2 +1,8 @@
-package com.capstone.liveAloneCommunity.repository.comment;public interface CommentRepository {
+package com.capstone.liveAloneCommunity.repository.comment;
+
+import com.capstone.liveAloneCommunity.controller.comment.CommentController;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<CommentController, Long> {
+
 }
