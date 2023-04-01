734e19ed4f7248ac5a4315505ee18d47c6acc446
eom-tae-in <eti0728@daum.net>
2023-04-01 21:32:35 +0900

    [feat] commentRepository 생성

--- src/main/java/com/capstone/liveAloneCommunity/service/comment/CommentService.java
+++ src/main/java/com/capstone/liveAloneCommunity/service/comment/CommentService.java
@@ -1,2 +1,14 @@
-package com.capstone.liveAloneCommunity.service.comment;public class CommentService {
+package com.capstone.liveAloneCommunity.service.comment;
+
+import com.capstone.liveAloneCommunity.repository.comment.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CommentService {
+
+    private CommentRepository commentRepository;
 }
