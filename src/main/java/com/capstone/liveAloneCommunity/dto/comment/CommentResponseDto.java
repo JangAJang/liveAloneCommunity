ec0a8d839077c3b7e63b74e8f2f614b455b1dabc
eom-tae-in <eti0728@daum.net>
2023-04-01 22:46:11 +0900

    [feat] CommentResponseDto 생성

--- src/main/java/com/capstone/liveAloneCommunity/dto/comment/CommentResponseDto.java
+++ src/main/java/com/capstone/liveAloneCommunity/dto/comment/CommentResponseDto.java
@@ -1,2 +1,14 @@
-package com.capstone.liveAloneCommunity.dto.comment;public class CommentResponseDto {
+package com.capstone.liveAloneCommunity.dto.comment;
+
+import com.capstone.liveAloneCommunity.entity.comment.Comment;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+public class CommentResponseDto {
+
+    private Long id;
+    private String content;
+    private String nickname;
+    private LocalDateTime createTime;
 }
