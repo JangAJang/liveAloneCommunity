5bb933ea1860927ff83277acb969cb53a9506abd
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-01 21:21:01 +0900

    [feat] 이메일을 생성하는 로직 구현

--- src/main/java/com/capstone/liveAloneCommunity/service/auth/EmailAuthService.java
+++ src/main/java/com/capstone/liveAloneCommunity/service/auth/EmailAuthService.java
@@ -1,10 +1,18 @@
 package com.capstone.liveAloneCommunity.service.auth;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
 import java.util.Random;
 import java.util.stream.IntStream;
+
+import static com.capstone.liveAloneCommunity.service.auth.EmailAuthComponent.SENDER;
+import static com.capstone.liveAloneCommunity.service.auth.EmailAuthComponent.TITLE;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +20,7 @@ import java.util.stream.IntStream;
 public class EmailAuthService {
 
     private StringBuilder authNum;
+    private final JavaMailSender mailSender;
 
     private void createCode(){
         authNum = new StringBuilder();
@@ -19,6 +28,15 @@ public class EmailAuthService {
         IntStream.range(0, 8).forEach(i -> authNum.append(createRandomCharacter(random)));
     }
 
+    public MimeMessage createEmailForm(String email) throws MessagingException, UnsupportedEncodingException {
+        createCode();
+        MimeMessage message = mailSender.createMimeMessage();
+        message.addRecipients(MimeMessage.RecipientType.TO, email);
+        message.setSubject(TITLE.getValue());
+        message.setFrom(SENDER.getValue());
+        message.setText("인증 번호는 " + authNum + "입니다.", "utf-8", "html");
+        return message;
+    }
     private char createRandomCharacter(Random random){
         int value = random.nextInt(3);
         if(value == 0) return (char) (random.nextInt(26)+97);
