5d9e94a6eeddbdd2faa231fbe1c5e90f804232d9
eom-tae-in <eti0728@daum.net>
2023-04-01 22:40:15 +0900

    [refactor] 댓글의 생성과 수정 일자를 남기기 위해 BaseTimeEntity클래스 extends 추가

--- src/main/java/com/capstone/liveAloneCommunity/entity/comment/Comment.java
+++ src/main/java/com/capstone/liveAloneCommunity/entity/comment/Comment.java
@@ -1,17 +1,21 @@
 package com.capstone.liveAloneCommunity.entity.comment;
 
 import com.capstone.liveAloneCommunity.domain.post.Content;
+import com.capstone.liveAloneCommunity.domain.post.Title;
+import com.capstone.liveAloneCommunity.entity.BaseTimeEntity;
 import com.capstone.liveAloneCommunity.entity.member.Member;
 import com.capstone.liveAloneCommunity.entity.post.Post;
+import com.querydsl.core.types.dsl.TimeTemplate;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Comment {
+public class Comment extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -27,4 +31,6 @@ public class Comment {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
+
+
 }
