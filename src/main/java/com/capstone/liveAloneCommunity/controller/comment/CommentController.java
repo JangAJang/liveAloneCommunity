bd5b42a5df938613d6be9ccd3d5f704a03d0e231
eom-tae-in <eti0728@daum.net>
2023-04-02 15:21:27 +0900

    [feat] 댓글 작성 컨트롤러 로직 구현

--- src/main/java/com/capstone/liveAloneCommunity/controller/comment/CommentController.java
+++ src/main/java/com/capstone/liveAloneCommunity/controller/comment/CommentController.java
@@ -1,9 +1,18 @@
 package com.capstone.liveAloneCommunity.controller.comment;
 
+import com.capstone.liveAloneCommunity.dto.comment.CommentResponseDto;
+import com.capstone.liveAloneCommunity.dto.comment.WriteCommentRequestDto;
+import com.capstone.liveAloneCommunity.entity.member.Member;
+import com.capstone.liveAloneCommunity.exception.member.MemberNotAllowedException;
+import com.capstone.liveAloneCommunity.repository.member.MemberRepository;
+import com.capstone.liveAloneCommunity.response.Response;
 import com.capstone.liveAloneCommunity.service.comment.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentController {
 
     private CommentService commentService;
+    private MemberRepository memberRepository;
+
+    @GetMapping("")
+    @Operation(summary = "댓글 작성", description = "게시물의 댓글을 작성한다.")
+    @ResponseStatus(HttpStatus.OK)
+    public Response writeComment(@Valid @RequestBody WriteCommentRequestDto writeCommentRequestDto) {
+        Member member = memberRepository.findByUsername_Username(SecurityContextHolder.getContext().getAuthentication().getName())
+                .orElseThrow(MemberNotAllowedException::new);
+        return Response.success(commentService.writeComment(writeCommentRequestDto, member));
+    }
 }
