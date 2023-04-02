739b5362c4324f5f881560199332fb4db22be8cc
eom-tae-in <eti0728@daum.net>
2023-04-02 23:33:30 +0900

    [style] 줄 맞춤 변경 && 띄어쓰기 변경

--- src/main/java/com/capstone/liveAloneCommunity/dto/comment/CommentResponseDto.java
+++ src/main/java/com/capstone/liveAloneCommunity/dto/comment/CommentResponseDto.java
@@ -2,15 +2,15 @@ package com.capstone.liveAloneCommunity.dto.comment;
 
 import com.capstone.liveAloneCommunity.entity.comment.Comment;
 import lombok.*;
-
 import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 @Builder
 public class CommentResponseDto {
 
-    private  Long id;
+    private Long id;
     private String content;
     private String nickname;
     private LocalDateTime createTime;
