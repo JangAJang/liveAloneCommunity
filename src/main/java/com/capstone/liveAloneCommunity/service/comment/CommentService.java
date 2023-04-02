7d30f8309b03aab002bcfa1ec7d99986ed7c1feb
eom-tae-in <eti0728@daum.net>
2023-04-02 15:20:55 +0900

    [feat] 댓글 작성 서비스 로직 구현

--- src/main/java/com/capstone/liveAloneCommunity/service/comment/CommentService.java
+++ src/main/java/com/capstone/liveAloneCommunity/service/comment/CommentService.java
@@ -1,6 +1,13 @@
 package com.capstone.liveAloneCommunity.service.comment;
 
+import com.capstone.liveAloneCommunity.dto.comment.CommentResponseDto;
+import com.capstone.liveAloneCommunity.dto.comment.WriteCommentRequestDto;
+import com.capstone.liveAloneCommunity.entity.comment.Comment;
+import com.capstone.liveAloneCommunity.entity.member.Member;
+import com.capstone.liveAloneCommunity.entity.post.Post;
+import com.capstone.liveAloneCommunity.exception.post.PostNotFoundException;
 import com.capstone.liveAloneCommunity.repository.comment.CommentRepository;
+import com.capstone.liveAloneCommunity.repository.post.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,4 +18,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommentService {
 
     private CommentRepository commentRepository;
+    private PostRepository postRepository;
+
+    public CommentResponseDto writeComment(WriteCommentRequestDto writeCommentRequestDto, Member member) {
+        Post post = postRepository.findById(writeCommentRequestDto.getPostId())
+                .orElseThrow(PostNotFoundException::new);
+        Comment comment = new Comment(writeCommentRequestDto.getContent(), post, member);
+        return CommentResponseDto.toDto(comment);
+    }
 }
