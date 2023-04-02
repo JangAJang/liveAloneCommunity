e4e76522ec7f596131f8253870f7b48b524effb5
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-02 23:21:19 +0900

    [refactor] 로그인을 요청했을 때 성공하는 경우 200코드와 access token을 반환하는 것을 검증하는 테스트코드 구현

---
+++ src/test/java/com/capstone/liveAloneCommunity/controller/auth/LogInTest.java
@@ -0,0 +1,69 @@
+package com.capstone.liveAloneCommunity.controller.auth;
+
+import com.capstone.liveAloneCommunity.dto.auth.LogInRequestDto;
+import com.capstone.liveAloneCommunity.dto.auth.RegisterRequestDto;
+import com.capstone.liveAloneCommunity.repository.member.MemberRepository;
+import com.capstone.liveAloneCommunity.service.auth.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LogInTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private AuthService authService;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void initDB(){
+        memberRepository.deleteAll();
+        RegisterRequestDto registerRequestDto = RegisterRequestDto.builder()
+                .username("former")
+                .nickname("former")
+                .email("former@former.com")
+                .password("former")
+                .passwordCheck("former")
+                .build();
+        authService.register(registerRequestDto);
+    }
+
+    @Test
+    @DisplayName("올바른 아이디와 비밀번호를 입력하면 로그인을 성공하며 토큰을 반환한다.")
+    public void logInTest_Success() throws Exception{
+        //given
+        LogInRequestDto logInRequestDto = LogInRequestDto.builder()
+                .username("former")
+                .password("former").build();
+        //expected
+        mvc.perform(MockMvcRequestBuilders.post("/api/auth/logIn")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(makeJson(logInRequestDto)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(200))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.data.accessToken").exists())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    private String makeJson(Object object){
+        try{
+            return new ObjectMapper().writeValueAsString(object);
+        }catch (Exception e){
+            return "";
+        }
+    }
+}
