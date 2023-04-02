997c757b9cc7d0447475dec59ec61ec32b3e17ec
eom-tae-in <eti0728@daum.net>
2023-04-03 02:43:02 +0900

    [feat] 게시물의 모든 댓글을 조회하는 쿼리 메서드 구현

--- src/main/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepositoryCustomImpl.java
+++ src/main/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepositoryCustomImpl.java
@@ -1,2 +1,27 @@
-package com.capstone.liveAloneCommunity.repository.comment;public class CommentRepositoryCustomImpl {
+package com.capstone.liveAloneCommunity.repository.comment;
+
+import com.capstone.liveAloneCommunity.entity.comment.Comment;
+import com.capstone.liveAloneCommunity.entity.comment.QComment;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.capstone.liveAloneCommunity.entity.comment.QComment.*;
+
+
+@RequiredArgsConstructor
+public class CommentRepositoryCustomImpl implements CommentRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<Comment> searchCommentByPostId(Long postId) {
+        return jpaQueryFactory
+                .select(comment)
+                .from(comment)
+                .where(comment.post.id.eq(postId))
+                .fetch();
+    }
 }
