4c6206567f874fd78487c7a8bd8b0a8e2106f3e3
eom-tae-in <eti0728@daum.net>
2023-04-03 02:45:33 +0900

    [test] CommentServiceTest 테스트 클래스 생성

--- src/test/java/com/capstone/liveAloneCommunity/service/comment/CommentServiceTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/service/comment/CommentServiceTest.java
@@ -1,10 +1,117 @@
 package com.capstone.liveAloneCommunity.service.comment;
 
+import com.capstone.liveAloneCommunity.dto.auth.RegisterRequestDto;
+import com.capstone.liveAloneCommunity.dto.comment.WriteCommentRequestDto;
+import com.capstone.liveAloneCommunity.dto.post.WritePostRequestDto;
+import com.capstone.liveAloneCommunity.entity.member.Member;
+import com.capstone.liveAloneCommunity.entity.post.Post;
+import com.capstone.liveAloneCommunity.exception.member.MemberNotFoundException;
+import com.capstone.liveAloneCommunity.exception.post.PostNotFoundException;
+import com.capstone.liveAloneCommunity.repository.comment.CommentRepository;
+import com.capstone.liveAloneCommunity.repository.member.MemberRepository;
+import com.capstone.liveAloneCommunity.repository.post.PostRepository;
+import com.capstone.liveAloneCommunity.service.auth.AuthService;
+import com.capstone.liveAloneCommunity.service.post.PostService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import static com.capstone.liveAloneCommunity.domain.post.Category.COOKING;
+import static com.capstone.liveAloneCommunity.domain.post.Category.HOBBY_SHARE;
 
 @SpringBootTest
 @Transactional
 class CommentServiceTest {
 
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @BeforeEach
+    void initTestData() {
+        IntStream.range(0, 10).forEach(i -> {
+            authService.register(RegisterRequestDto.builder()
+                    .username("test" + i)
+                    .nickname("test" + i)
+                    .email("test" + i + "@test.com")
+                    .password("test")
+                    .passwordCheck("test")
+                    .build());
+            Member member = memberRepository.findByUsername_Username("test" + i).orElseThrow(MemberNotFoundException::new);
+            IntStream.range(i * 10, i * 10 + 5).forEach(index ->
+            {
+                postService.writePost(member, WritePostRequestDto.builder()
+                        .category(COOKING)
+                        .title("title" + index)
+                        .content("content" + index).build());
+                Post post = postRepository.findByTitle_Title("title" + index)
+                        .orElseThrow(PostNotFoundException::new);
+                IntStream.range(0, 100).forEach(comment ->
+                {
+                    commentService.writeComment(new WriteCommentRequestDto(post.getId(), "commentTest" + comment), member);
+                });
+            });
+            IntStream.range(i * 10 + 5, i * 10 + 10).forEach(index ->
+            {
+                postService.writePost(member, WritePostRequestDto.builder()
+                        .category(HOBBY_SHARE)
+                        .title("title" + index)
+                        .content("content" + index).build());
+                Post post = postRepository.findByTitle_Title("title" + index)
+                        .orElseThrow(PostNotFoundException::new);
+                IntStream.range(0, 100).forEach(comment ->
+                {
+                    commentService.writeComment(new WriteCommentRequestDto(post.getId(), "commentTest" + comment), member);
+                });
+            });
+        });
+    }
+
+
+    @Test
+    @DisplayName("게시물의 id와 댓글 내용으로 댓글을 생성한다. ")
+    void createComment() {
+        //given
+        authService.register(RegisterRequestDto.builder()
+                .username("test")
+                .nickname("test")
+                .email("test@test.com")
+                .password("test")
+                .passwordCheck("test").build());
+        Member member = memberRepository.findByUsername_Username("test").orElseThrow(MemberNotFoundException::new);
+        Random random = new Random();
+        Post post = postRepository.findById(random.nextLong(0, 99))
+                .orElseThrow(PostNotFoundException::new);
+        WriteCommentRequestDto writeCommentRequestDto = new WriteCommentRequestDto(post.getId(), "testCase~~!!");
+
+        //when
+        commentService.writeComment(writeCommentRequestDto, member);
+
+        //then
+        Assertions.assertThat(commentRepository.searchCommentByPostId(post.getId()).size())
+                .isEqualTo(101);
+    }
+
 }
+
