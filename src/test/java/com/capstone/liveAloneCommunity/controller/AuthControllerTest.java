6ddea94a6822389a3884895f4ceb51a987c1db10
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-02 22:04:01 +0900

    [feat] 이메일 형식이 올바르지 않을 때 회원가입을 실패하는 것을 검증하는 테스트코드 구현

--- src/test/java/com/capstone/liveAloneCommunity/controller/AuthControllerTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/controller/AuthControllerTest.java
@@ -3,6 +3,7 @@ package com.capstone.liveAloneCommunity.controller;
 import com.capstone.liveAloneCommunity.dto.auth.RegisterRequestDto;
 import com.capstone.liveAloneCommunity.repository.member.MemberRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,11 @@ public class AuthControllerTest {
     @Autowired
     private MemberRepository memberRepository;
 
+    @BeforeEach
+    void clearDB(){
+        memberRepository.deleteAll();
+    }
+
     @Test
     @DisplayName("회원가입 성공시, 200코드가 반환된다.")
     public void registerTest_Success() throws Exception{
@@ -41,6 +47,28 @@ public class AuthControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(true))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(200))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("회원가입 실행시 이메일 형식이 올바르지 않으면 예외처리한다. ")
+    public void registerTest_FAIL_EMAIL_FORMAT() throws Exception{
+        //given
+        RegisterRequestDto registerRequestDto = RegisterRequestDto.builder()
+                .username("test")
+                .nickname("test")
+                .email("testtest.com")
+                .password("test")
+                .passwordCheck("test")
+                .build();
+        //expected
+        mvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(makeJson(registerRequestDto)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(400))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.failMessage").value("올바르지 않은 이메일 형식입니다."))
                 .andDo(MockMvcResultHandlers.print());
     }
 
