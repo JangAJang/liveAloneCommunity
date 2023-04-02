01df4752cd896592247580912e4159ef3ccc0c9c
eom-tae-in <eti0728@daum.net>
2023-04-03 03:24:00 +0900

    [test] 댓글 생성시 Comment Entity 초기화가 잘 되는지 확인하는 테스트 메서드 구현

--- src/test/java/com/capstone/liveAloneCommunity/entity/CommentTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/entity/CommentTest.java
@@ -1,7 +1,38 @@
 package com.capstone.liveAloneCommunity.entity;
 
+import com.capstone.liveAloneCommunity.domain.member.MemberInfo;
+import com.capstone.liveAloneCommunity.domain.member.Password;
+import com.capstone.liveAloneCommunity.domain.member.Username;
+import com.capstone.liveAloneCommunity.domain.post.Category;
+import com.capstone.liveAloneCommunity.domain.post.Content;
+import com.capstone.liveAloneCommunity.domain.post.Title;
+import com.capstone.liveAloneCommunity.entity.comment.Comment;
+import com.capstone.liveAloneCommunity.entity.member.Member;
+import com.capstone.liveAloneCommunity.entity.member.Role;
+import com.capstone.liveAloneCommunity.entity.post.Post;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
 
 @Transactional
 class CommentTest {
+
+    @Test
+    @DisplayName("댓글을 생성했을 때 댓글의 필드가 초기화된다.")
+    public void createComment() {
+        //given
+        Member member = new Member(new Username("test1"), new MemberInfo("test2", "email@email.com"),
+                new Password("test3"), Role.USER);
+        Post post = new Post(new Title("title"), new Content("content"), member, Category.COOKING);
+
+        //when
+        Comment comment = new Comment("test", post, member);
+
+        //then
+        assertThat(comment.getContent()).isEqualTo("test");
+        assertThat(comment.getPost()).isEqualTo(post);
+        assertThat(comment.getMember()).isEqualTo(member);
+    }
 }
