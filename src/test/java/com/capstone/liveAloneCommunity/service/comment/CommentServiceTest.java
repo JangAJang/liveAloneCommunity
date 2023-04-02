61ffb17ada530463e94d2bb02eaddd2698d0d65a
eom-tae-in <eti0728@daum.net>
2023-04-02 21:38:23 +0900

    [feat] CommentServieTest 클래스 생성(initial commit)

---
+++ src/test/java/com/capstone/liveAloneCommunity/service/comment/CommentServiceTest.java
@@ -0,0 +1,10 @@
+package com.capstone.liveAloneCommunity.service.comment;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class CommentServiceTest {
+
+}
