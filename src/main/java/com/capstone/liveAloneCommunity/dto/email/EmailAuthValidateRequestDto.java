bbbd135c740280f74253a268cbbd84be827f4951
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-03 22:37:08 +0900

    [feat] 입력이 null, 공백, 빈 문자열로 올 것을 감안해 Validation어노테이션을 활용해 예외를 발생시키게 코드 추가

--- src/main/java/com/capstone/liveAloneCommunity/dto/email/EmailAuthValidateRequestDto.java
+++ src/main/java/com/capstone/liveAloneCommunity/dto/email/EmailAuthValidateRequestDto.java
@@ -1,5 +1,8 @@
 package com.capstone.liveAloneCommunity.dto.email;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,6 +13,12 @@ import lombok.NoArgsConstructor;
 @Getter
 public class EmailAuthValidateRequestDto {
 
+    @NotNull(message = "이메일을 입력해주세요.")
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @NotEmpty(message = "이메일을 입력해주세요.")
     private String email;
+    @NotNull(message = "인증번호를 입력해주세요.")
+    @NotBlank(message = "인증번호를 입력해주세요.")
+    @NotEmpty(message = "인증번호를 입력해주세요.")
     private String authNum;
 }
