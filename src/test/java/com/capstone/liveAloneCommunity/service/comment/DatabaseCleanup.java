52757a085e6133b06a65a16b365f7e902c856ba1
eom-tae-in <eti0728@daum.net>
2023-04-05 06:42:59 +0900

    [test] EntityManager에서 @Entity가 붙은 클래스들만 걸러서 LowerUnderScore로 바꿔서  리스트에 저장하는 메서드 생성

--- src/test/java/com/capstone/liveAloneCommunity/service/comment/DatabaseCleanup.java
+++ src/test/java/com/capstone/liveAloneCommunity/service/comment/DatabaseCleanup.java
@@ -1,15 +1,27 @@
 package com.capstone.liveAloneCommunity.service.comment;
 
+import com.google.common.base.CaseFormat;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DatabaseCleanup{
+
     @PersistenceContext
     private EntityManager entityManager;
 
     private List<String> tableNames;
+
+    @PostConstruct
+    public void afterPropertiesSet() {
+        tableNames = entityManager.getMetamodel().getEntities().stream()
+                .filter(e -> e.getJavaType().getAnnotation(Entity.class) != null)
+                .map(e -> CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, e.getName()))
+                .collect(Collectors.toList());
+    }
 }
