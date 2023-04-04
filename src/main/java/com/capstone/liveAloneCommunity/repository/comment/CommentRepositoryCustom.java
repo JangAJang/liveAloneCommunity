6b6acf39d8fd1280ceb2fc1c8811520ca783842e
eom-tae-in <eti0728@daum.net>
2023-04-05 01:36:49 +0900

    [style] 불필요한 개행 삭제

--- src/main/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepositoryCustom.java
+++ src/main/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepositoryCustom.java
@@ -1,7 +1,6 @@
 package com.capstone.liveAloneCommunity.repository.comment;
 
 import com.capstone.liveAloneCommunity.entity.comment.Comment;
-
 import java.util.List;
 
 public interface CommentRepositoryCustom {
