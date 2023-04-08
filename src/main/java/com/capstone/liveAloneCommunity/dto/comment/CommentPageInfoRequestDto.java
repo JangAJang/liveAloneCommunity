b22a227168174b7e1cac5d6dd7a73e05632f4adc
eom-tae-in <eti0728@daum.net>
2023-04-08 15:38:34 +0900

    [feat] 유저의 현재 page, size 정보를 받기 위한 RequestDto 추가

--- src/main/java/com/capstone/liveAloneCommunity/dto/comment/CommentPageInfoRequestDto.java
+++ src/main/java/com/capstone/liveAloneCommunity/dto/comment/CommentPageInfoRequestDto.java
@@ -1,2 +1,14 @@
-package com.capstone.liveAloneCommunity.dto.comment;public class CommentPageInfoRequestDto {
+package com.capstone.liveAloneCommunity.dto.comment;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class CommentPageInfoRequestDto {
+    @NotNull(message = "페이지를 입력해주세요")
+    private Integer page;
+    @NotNull(message = "size를 입력해주세요")
+    private Integer size;
 }
