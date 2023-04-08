2ebfadeae867ecd13fb7fdb4f65e122e2aaae7c8
eom-tae-in <eti0728@daum.net>
2023-04-08 13:28:39 +0900

    [feat] 게시물의 id를 받는 RequestDto 추가

--- src/main/java/com/capstone/liveAloneCommunity/dto/comment/ReadCommentByPostRequestDto.java
+++ src/main/java/com/capstone/liveAloneCommunity/dto/comment/ReadCommentByPostRequestDto.java
@@ -1,2 +1,14 @@
-package com.capstone.liveAloneCommunity.dto.comment;public class ReadCommentByPostRequestDto {
+package com.capstone.liveAloneCommunity.dto.comment;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReadCommentByPostRequestDto {
+    @NotNull(message = "게시물의 id를 입력해주세요.")
+    private Long postId;
 }
