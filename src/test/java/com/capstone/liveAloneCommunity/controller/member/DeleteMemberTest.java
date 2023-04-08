88b4589dba05385f392e8bff42d13c006a37c82a
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-08 19:09:45 +0900

    [test] 회원 삭제시에 토큰이 존재하지 않으면 401에러와 다시 로그인해야함을 알려주는 것을 검증하는 테스트코드 구현

--- src/test/java/com/capstone/liveAloneCommunity/controller/member/DeleteMemberTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/controller/member/DeleteMemberTest.java
@@ -49,6 +49,19 @@ public class DeleteMemberTest {
                 .andDo(MockMvcResultHandlers.print());
     }
 
+    @Test
+    @DisplayName("회원을 삭제할 때 토큰이 있으면 200코드와 함꼐 성공적으로 회원이 삭제됨을 반환한다.")
+    public void deleteMemberTest_Fail_Unauthorized() throws Exception{
+        //given
+        //expected
+        mvc.perform(MockMvcRequestBuilders.delete("/api/member/delete"))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(401))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.failMessage").value("다시 로그인해주세요."))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
     private String getAccessTokenAfterLogIn(){
         return authService.logIn(LogInRequestDto.builder().username("test").password("test").build()).getAccessToken();
     }
