7272c5161505b866d1e884072ba9a6743a86c251
eom-tae-in <eti0728@daum.net>
2023-04-08 23:14:41 +0900

    [refactor] 검증을 위한 @Valid와 @NotNull 추가

--- src/main/java/com/capstone/liveAloneCommunity/dto/comment/ReadCommentByPostRequestDto.java
+++ src/main/java/com/capstone/liveAloneCommunity/dto/comment/ReadCommentByPostRequestDto.java
@@ -1,5 +1,6 @@
 package com.capstone.liveAloneCommunity.dto.comment;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,5 +13,6 @@ public class ReadCommentByPostRequestDto {
     @NotNull(message = "게시물의 id를 입력해주세요.")
     private Long postId;
 
+    @Valid @NotNull(message = "page와 size을 다시 입력해주세요")
     private CommentPageInfoRequestDto commentPageInfoRequestDto;
 }
