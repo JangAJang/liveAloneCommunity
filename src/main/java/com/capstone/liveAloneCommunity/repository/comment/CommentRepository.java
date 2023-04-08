b7e4636f52a5f74566c5f08c341e88fedf332906
eom-tae-in <eti0728@daum.net>
2023-04-08 12:06:34 +0900

    [feat] 멤버가 작성한 댓글을 가져오는 로직 추가

--- src/main/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepository.java
+++ src/main/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepository.java
@@ -1,8 +1,10 @@
 package com.capstone.liveAloneCommunity.repository.comment;
 
 import com.capstone.liveAloneCommunity.entity.comment.Comment;
+import com.capstone.liveAloneCommunity.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
-
+    List<Comment> findCommentByMember(Member member);
 }
