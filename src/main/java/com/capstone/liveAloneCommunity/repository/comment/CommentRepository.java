3dc2603ebb61d75a6ea78adf82096a8d68515f94
eom-tae-in <eti0728@daum.net>
2023-04-08 15:41:48 +0900

    [refactor] 페이징 처리를 적용하기 위한 레포지토리 코드 수정

--- src/main/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepository.java
+++ src/main/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepository.java
@@ -2,12 +2,12 @@ package com.capstone.liveAloneCommunity.repository.comment;
 
 import com.capstone.liveAloneCommunity.entity.comment.Comment;
 import com.capstone.liveAloneCommunity.entity.member.Member;
-import com.capstone.liveAloneCommunity.entity.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
 
-    List<Comment> findCommentByMember(Member member);
-    List<Comment> findCommentByPostId(Long postId);
+    Page<Comment> findCommentByMember(Member member, Pageable pageable);
+    Page<Comment> findCommentByPostId(Long postId, Pageable pageable);
 }
