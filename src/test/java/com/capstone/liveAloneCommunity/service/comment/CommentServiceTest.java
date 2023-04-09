093414305dc7fae922a6f0272dde20d511d8140b
eom-tae-in <eti0728@daum.net>
2023-04-10 00:22:06 +0900

    [test] member의 id로 회원의 댓글을 조회하는 테스트 추가

--- src/test/java/com/capstone/liveAloneCommunity/service/comment/CommentServiceTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/service/comment/CommentServiceTest.java
@@ -21,9 +21,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @ExtendWith(MockitoExtension.class)
 class CommentServiceTest {
@@ -61,6 +72,29 @@ class CommentServiceTest {
         //when //then
         assertThatThrownBy(() -> commentService.writeComment(writeCommentRequestDto, member))
                 .isInstanceOf(PostNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("member의 id로 회원의 댓글을 조회한다.")
+    void readCommentByMemberTest() {
+        //given
+        Member member1 = createMember(1);
+        Member member2 = createMember(2);
+        Post post = createPost(member2, 1);
+        List<Comment> comments = new ArrayList<>();
+        IntStream.range(0, 999).forEach(i -> comments.add(createComment(member1, post, i)));
+        IntStream.range(0, 888).forEach(i -> comments.add(createComment(member2, post, i)));
+        CommentPageInfoRequestDto commentPageInfoRequestDto = new CommentPageInfoRequestDto(0, 10);
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(DESC,"createdDate"));
+        Page<Comment> readCommentByMember = new PageImpl<>(comments.stream().filter(comment ->
+                comment.getMember().equals(member1)).collect(Collectors.toList()));
+        given(commentRepository.findCommentByMember(member1, pageRequest)).willReturn(readCommentByMember);
+
+        //when
+        MultiReadCommentResponseDto multiReadCommentResponseDto = commentService.readCommentByMember(member1, commentPageInfoRequestDto);
+
+        //then
+        assertThat(multiReadCommentResponseDto.getReadCommentResponseDto().size()).isEqualTo( 999);
     }
 
     private Member createMember(int id) {
