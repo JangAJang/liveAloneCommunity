5e3ab18af91da6efd510cbd5e97e436b74af0150
eom-tae-in <eti0728@daum.net>
2023-04-11 13:57:59 +0900

    [feat] 수정할 댓글 내용과 댓글의 id를 받는 RequsetDto 생성

---
+++ src/main/java/com/capstone/liveAloneCommunity/dto/comment/EditCommentRequestDto.java
@@ -0,0 +1,13 @@
+package com.capstone.liveAloneCommunity.dto.comment;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class EditCommentRequestDto {
+    @NotNull(message = "댓글의 id를 입력해주세요.")
+    private Long commentId;
+    @NotBlank(message = "변경할 댓글의 내용을 입력해주세요.")
+    private String modifyContent;
+}
