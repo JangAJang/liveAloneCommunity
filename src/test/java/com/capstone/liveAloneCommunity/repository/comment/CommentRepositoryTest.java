9dc4029d8623392177534c3541bc67607d318e75
eom-tae-in <eti0728@daum.net>
2023-04-11 11:38:17 +0900

    [test] member를 생성하는 로직 메서드로 분리

--- src/test/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepositoryTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepositoryTest.java
@@ -2,13 +2,23 @@ package com.capstone.liveAloneCommunity.repository.comment;
 
 import com.capstone.liveAloneCommunity.DatabaseCleanup;
 import com.capstone.liveAloneCommunity.config.querydsl.QuerydslConfig;
+import com.capstone.liveAloneCommunity.domain.member.Email;
+import com.capstone.liveAloneCommunity.domain.member.Nickname;
+import com.capstone.liveAloneCommunity.domain.member.Password;
+import com.capstone.liveAloneCommunity.domain.member.Username;
+import com.capstone.liveAloneCommunity.entity.member.Member;
+import com.capstone.liveAloneCommunity.entity.member.Role;
 import com.capstone.liveAloneCommunity.repository.member.MemberRepository;
 import com.capstone.liveAloneCommunity.repository.post.PostRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.*;
 import org.springframework.context.annotation.Import;
+
+import static com.capstone.liveAloneCommunity.entity.member.Role.*;
 
 @Import({QuerydslConfig.class, DatabaseCleanup.class})
 @DataJpaTest
@@ -26,5 +36,15 @@ class CommentRepositoryTest {
     @AfterEach
     void afterTestCleanData() {
         databaseCleanup.execute();
+    }
+
+    private Member createMember() {
+        return Member.builder()
+                .username(new Username("test"))
+                .nickname(new Nickname("test"))
+                .email(new Email("test@email.com"))
+                .password(new Password("test"))
+                .role(USER)
+                .build();
     }
 }
