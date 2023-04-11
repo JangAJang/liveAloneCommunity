72459696220162db41513c3b9df3af7241bf3ac5
eom-tae-in <eti0728@daum.net>
2023-04-11 18:24:48 +0900

    [feat] 삭제할 댓글의 id를 받는 RequestDto 추가

--- src/main/java/com/capstone/liveAloneCommunity/dto/comment/DeleteCommentRequestDto.java
+++ src/main/java/com/capstone/liveAloneCommunity/dto/comment/DeleteCommentRequestDto.java
@@ -1,2 +1,15 @@
-package com.capstone.liveAloneCommunity.dto.comment;public class DeleteRequestDto {
+package com.capstone.liveAloneCommunity.dto.comment;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DeleteCommentRequestDto {
+    @NotNull
+    private Long commentId;
 }
