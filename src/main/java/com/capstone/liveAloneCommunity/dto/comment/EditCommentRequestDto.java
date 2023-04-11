bc121a2f323d8cd02aef043f35354d069555be8b
eom-tae-in <eti0728@daum.net>
2023-04-11 16:07:22 +0900

    [refactor] 테스트코드에서 값을 넣어서 생성하는 경우가 있어서 @AllArgsConstructor 추가 && JSON을 객체타입으로 파싱할 때 @NoArgsConstructor가 필요하여 추가

--- src/main/java/com/capstone/liveAloneCommunity/dto/comment/EditCommentRequestDto.java
+++ src/main/java/com/capstone/liveAloneCommunity/dto/comment/EditCommentRequestDto.java
@@ -2,9 +2,14 @@ package com.capstone.liveAloneCommunity.dto.comment;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EditCommentRequestDto {
     @NotNull(message = "댓글의 id를 입력해주세요.")
     private Long commentId;
