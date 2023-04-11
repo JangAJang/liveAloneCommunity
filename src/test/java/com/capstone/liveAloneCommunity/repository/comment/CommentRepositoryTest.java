f7981c8894eb2266548103985c6ed179a6862eed
eom-tae-in <eti0728@daum.net>
2023-04-11 11:44:29 +0900

    [test] 댓글 저장을 테스트하는 단위 테스트 추가

--- src/test/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepositoryTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/repository/comment/CommentRepositoryTest.java
@@ -9,11 +9,13 @@ import com.capstone.liveAloneCommunity.domain.member.Username;
 import com.capstone.liveAloneCommunity.domain.post.Category;
 import com.capstone.liveAloneCommunity.domain.post.Content;
 import com.capstone.liveAloneCommunity.domain.post.Title;
+import com.capstone.liveAloneCommunity.entity.comment.Comment;
 import com.capstone.liveAloneCommunity.entity.member.Member;
 import com.capstone.liveAloneCommunity.entity.member.Role;
 import com.capstone.liveAloneCommunity.entity.post.Post;
 import com.capstone.liveAloneCommunity.repository.member.MemberRepository;
 import com.capstone.liveAloneCommunity.repository.post.PostRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,6 +26,7 @@ import org.springframework.context.annotation.Import;
 
 import static com.capstone.liveAloneCommunity.domain.post.Category.*;
 import static com.capstone.liveAloneCommunity.entity.member.Role.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Import({QuerydslConfig.class, DatabaseCleanup.class})
 @DataJpaTest
@@ -41,6 +44,24 @@ class CommentRepositoryTest {
     @AfterEach
     void afterTestCleanData() {
         databaseCleanup.execute();
+    }
+
+    @Test
+    @DisplayName("댓글이 데이터베이스에 잘 저장되는지 확인한다.")
+    void saveCommentTest() {
+        //given
+        Member member = createMember();
+        memberRepository.save(member);
+        Post post = createPost(member);
+        postRepository.save(post);
+        Comment comment = new Comment("comment", post, member);
+
+        //when
+        Comment save = commentRepository.save(comment);
+
+        //then
+        assertThat(comment.getPost()).isEqualTo(save.getPost());
+        assertThat(comment.getMember()).isEqualTo(save.getMember());
     }
 
     private Member createMember() {
