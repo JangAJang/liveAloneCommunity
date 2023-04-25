6a416a043c41f9404b11f9de4c28043d570a9426
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-25 22:12:16 +0900

    [feat] 채팅방과 채팅방을 다대다로 매핑하기 위해 중간 테이블 MemberInRoom을 구현

---
+++ chat/src/main/java/com/capstone/chat/entity/memberInRoom/MemberInRoom.java
@@ -0,0 +1,25 @@
+package com.capstone.chat.entity.memberInRoom;
+
+import com.capstone.chat.entity.chatRoom.ChatRoom;
+import com.capstone.chat.entity.member.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class MemberInRoom {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ChatRoom chatRoom;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+}
