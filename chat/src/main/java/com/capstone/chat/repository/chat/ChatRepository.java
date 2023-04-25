878d0eaa1e5f272392058346742fbbcb5033ad70
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-25 22:24:14 +0900

    [feat] 서버 포트 수정 && 채팅방을 이용해 채팅들을 찾는 리포지토리 구현

---
+++ chat/src/main/java/com/capstone/chat/repository/chat/ChatRepository.java
@@ -0,0 +1,13 @@
+package com.capstone.chat.repository.chat;
+
+import com.capstone.chat.entity.chat.Chat;
+import com.capstone.chat.entity.chatRoom.ChatRoom;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+
+@NoRepositoryBean
+public interface ChatRepository extends ReactiveCrudRepository<Chat, Long> {
+
+    Flux<Chat> findByChatRoomOrderBySentAtAsc(ChatRoom chatRoom);
+}
