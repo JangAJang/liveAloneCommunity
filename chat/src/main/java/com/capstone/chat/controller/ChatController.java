accefca0001b98be652d89ac68e9329253d419a3
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-27 01:59:39 +0900

    [feat] 내 모든 채팅을 조회하고, 그중에서 원하는 채팅방을 검색한 후 해당 채팅방에서 글을 보내기 까지의 로직을 수행하는 컨트롤러 구현

---
+++ chat/src/main/java/com/capstone/chat/controller/ChatController.java
@@ -0,0 +1,49 @@
+package com.capstone.chat.controller;
+
+import com.capstone.chat.dto.chat.ChatResponseDto;
+import com.capstone.chat.dto.chat.IntoRoomRequestDto;
+import com.capstone.chat.dto.chat.SendChatRequestDto;
+import com.capstone.chat.dto.chatRoom.MyChatRoomRequestDto;
+import com.capstone.chat.dto.chatRoom.SearchChatRoomRequestDto;
+import com.capstone.chat.entity.chatRoom.ChatRoom;
+import com.capstone.chat.service.ChatRoomService;
+import com.capstone.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+    private final ChatRoomService chatRoomService;
+
+    @CrossOrigin
+    @PostMapping(value = "/", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Mono<ChatResponseDto> sendMessage(@RequestBody @Validated SendChatRequestDto sendChatRequestDto){
+        return chatService.sendMessage(sendChatRequestDto);
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/rooms", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ChatRoom> getMyRooms(@RequestBody @Validated MyChatRoomRequestDto myChatRoomRequestDto){
+        return chatRoomService.findMyChatRooms(myChatRoomRequestDto);
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/rooms/search", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ChatRoom> searchRooms(@RequestBody @Validated SearchChatRoomRequestDto searchChatRoomRequestDto){
+        return chatRoomService.searchMyChatRooms(searchChatRoomRequestDto);
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ChatResponseDto> getInRoom(@RequestBody @Validated IntoRoomRequestDto intoRoomRequestDto){
+        return chatService.getIntoRoom(intoRoomRequestDto);
+    }
+}
