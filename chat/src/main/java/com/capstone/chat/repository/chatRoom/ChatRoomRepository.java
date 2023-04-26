79c3402a7472f3637eea3590e1453b012dd81bc3
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-26 15:04:27 +0900

    [feat] 채팅방을 찾아주는 리포지토리 생성

---
+++ chat/src/main/java/com/capstone/chat/repository/chatRoom/ChatRoomRepository.java
@@ -0,0 +1,9 @@
+package com.capstone.chat.repository.chatRoom;
+
+import com.capstone.chat.entity.chatRoom.ChatRoom;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+@NoRepositoryBean
+public interface ChatRoomRepository extends ReactiveCrudRepository<ChatRoom, Long> {
+}
