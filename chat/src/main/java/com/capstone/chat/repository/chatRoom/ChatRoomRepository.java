41c9b863fb157ee9299d601cfedbd14374bef872
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-05-02 21:31:05 +0900

    [refactor] JPA처럼 연결된 외래키를 수정하며 리포지토리 기능을 쿼리 직접 구현으로 수정

--- chat/src/main/java/com/capstone/chat/repository/chatRoom/ChatRoomRepository.java
+++ chat/src/main/java/com/capstone/chat/repository/chatRoom/ChatRoomRepository.java
@@ -1,8 +1,19 @@
 package com.capstone.chat.repository.chatRoom;
 
 import com.capstone.chat.entity.chatRoom.ChatRoom;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface ChatRoomRepository extends ReactiveCrudRepository<ChatRoom, Long> {
+
+    @Query("select * from chat_room c where c.id in (select m.chat_room_id from member_in_room m where m.member_id = :memberId)")
+    Flux<ChatRoom> findMembersChatRoom(@Param("memberId") Long memberId);
+    @Query("select m from member_in_room m where m.member_id = :senderId and m.chat_room_id in (" +
+            "select n.chat_room_id from member_in_room n where n.member_id = :receiverId")
+    Mono<ChatRoom> findChatRoomBySenderAndReceiver(@Param("senderId") Long senderId, @Param("receiverId") Long receiverId);
+
 }
--- chat/src/main/java/com/capstone/chat/repository/memberInRoom/MemberInRoomRepository.java
+++ chat/src/main/java/com/capstone/chat/repository/memberInRoom/MemberInRoomRepository.java
@@ -3,7 +3,9 @@ package com.capstone.chat.repository.memberInRoom;
 import com.capstone.chat.entity.chatRoom.ChatRoom;
 import com.capstone.chat.entity.member.Member;
 import com.capstone.chat.entity.memberInRoom.MemberInRoom;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
@@ -11,7 +13,7 @@ import java.util.List;
 
 public interface MemberInRoomRepository extends ReactiveCrudRepository<MemberInRoom, Long> {
 
-    Flux<MemberInRoom> findMemberInRoomByChatRoomId(Long chatRoomId);
-    List<MemberInRoom> findByMemberId(Member member);
-    Flux<MemberInRoom> findFluxByMemberId(Long memberId);
+    Flux<MemberInRoom> findMemberInRoomByChatRoom(ChatRoom chatRoom);
+    List<MemberInRoom> findByMember(Member member);
+    Flux<MemberInRoom> findFluxByMember(Member member);
 }
