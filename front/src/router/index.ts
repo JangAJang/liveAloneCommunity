b4750fd603787ec94f8a95c04114d81d5e5625ff
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-05-08 02:45:29 +0900

    [feat] 회원가입을 위해 구성되는 화면 구현

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import App from '@/App.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: App
+    },
+    {
+      path: '/auth/register',
+      name: 'register',
+      component: () => import('../components/auth/Register.vue'),
+      meta: { unauthorized: true }
+    }
+  ]
 })
 
 export default router
