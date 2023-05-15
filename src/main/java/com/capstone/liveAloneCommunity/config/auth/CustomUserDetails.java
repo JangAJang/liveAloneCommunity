3e8345242c2ca8c183d84be40c5202a65210f622
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-05-15 19:19:02 +0900

    [feat] 카카오, 네이버, 구글에 대한 UserInfo추가

---
+++ src/main/java/com/capstone/liveAloneCommunity/config/auth/CustomUserDetails.java
@@ -0,0 +1,73 @@
+package com.capstone.liveAloneCommunity.config.auth;
+
+import com.capstone.liveAloneCommunity.entity.member.Member;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@Data
+public class CustomUserDetails implements UserDetails , OAuth2User {
+
+    private Member member;
+
+    public CustomUserDetails(Member member){
+        this.member = member;
+    }
+
+    public CustomUserDetails(Member member, Map<String, Object> attributes) {
+
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(member.getRole()::name);
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
