94f8a046bb1111f5ad06120b6f6a8c4589a17368
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-05-16 22:01:53 +0900

    [feat] 카카오 로그인을 요청하면 개인의 코드값을 받아와 로그인 요청 dto를 받아오고, 이를 기존의 로그인 로직을 수행하는 기능 구현

--- src/main/java/com/capstone/liveAloneCommunity/controller/auth/AuthController.java
+++ src/main/java/com/capstone/liveAloneCommunity/controller/auth/AuthController.java
@@ -1,18 +1,27 @@
 package com.capstone.liveAloneCommunity.controller.auth;
 
 import com.capstone.liveAloneCommunity.config.jwt.TokenPath;
+import com.capstone.liveAloneCommunity.config.oauth.provider.KakaoProfile;
 import com.capstone.liveAloneCommunity.dto.auth.LogInRequestDto;
 import com.capstone.liveAloneCommunity.dto.auth.RegisterRequestDto;
 import com.capstone.liveAloneCommunity.dto.token.ReissueRequestDto;
 import com.capstone.liveAloneCommunity.dto.token.TokenResponseDto;
+import com.capstone.liveAloneCommunity.entity.token.OAuthToken;
+import com.capstone.liveAloneCommunity.exception.authentication.LogInAgainException;
 import com.capstone.liveAloneCommunity.response.Response;
 import com.capstone.liveAloneCommunity.service.auth.AuthService;
+import com.capstone.liveAloneCommunity.service.auth.KakaoAuthService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -21,6 +30,7 @@ public class AuthController {
 
     private final AuthService authService;
     private final TokenPath tokenPath;
+    private final KakaoAuthService kakaoAuthService;
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.OK)
@@ -37,6 +47,14 @@ public class AuthController {
         TokenResponseDto tokenResponseDto = authService.logIn(logInRequestDto);
         tokenPath.putAccessTokenOnHeader(tokenResponseDto.getAccessToken(), response);
         return Response.success();
+    }
+
+    @GetMapping("/kakao/callback")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "카카오 로그인", description = "카카오 로그인 진행, 만약 기존에 로그인한 적이 없다면 계정을 만든 후 진행")
+    public Response kakaoCallBack(@RequestParam("code") String code, HttpServletResponse response){
+        LogInRequestDto logInRequestDto = kakaoAuthService.getLogInRequestByCode(code);
+        return logIn(logInRequestDto, response);
     }
 
     @PatchMapping("/reissue")
