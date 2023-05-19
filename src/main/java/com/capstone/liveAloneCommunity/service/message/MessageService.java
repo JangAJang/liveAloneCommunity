c384a99bfc8313cc1009e26e5e751cc3ecdf40d4
eom-tae-in <eti0728@daum.net>
2023-05-20 00:09:49 +0900

    [feat] messageService에 단건 메세지 조회 기능 추가

--- src/main/java/com/capstone/liveAloneCommunity/service/message/MessageService.java
+++ src/main/java/com/capstone/liveAloneCommunity/service/message/MessageService.java
@@ -4,6 +4,7 @@ import com.capstone.liveAloneCommunity.dto.message.MessageResponseDto;
 import com.capstone.liveAloneCommunity.dto.message.WriteMessageRequestDto;
 import com.capstone.liveAloneCommunity.entity.member.Member;
 import com.capstone.liveAloneCommunity.entity.message.Message;
+import com.capstone.liveAloneCommunity.exception.message.MessageNotFoundException;
 import com.capstone.liveAloneCommunity.repository.message.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,11 @@ public class MessageService {
     public MessageResponseDto writeMessage(Member member, Member receiver, WriteMessageRequestDto writeMessageRequestDto) {
         Message message = new Message(member, receiver, writeMessageRequestDto.getContent());
         messageRepository.save(message);
+        return MessageResponseDto.toDto(message);
+    }
+
+    public MessageResponseDto readMessage(Long id) {
+        Message message = messageRepository.findById(id).orElseThrow(MessageNotFoundException::new);
         return MessageResponseDto.toDto(message);
     }
 }
