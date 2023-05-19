5f23a77651802b301f7be3275ec827606387d0bf
eom-tae-in <eti0728@daum.net>
2023-05-19 23:09:59 +0900

    [feat] 메세지를 작성 기능 구현

--- src/main/java/com/capstone/liveAloneCommunity/controller/message/MessageController.java
+++ src/main/java/com/capstone/liveAloneCommunity/controller/message/MessageController.java
@@ -1,9 +1,15 @@
 package com.capstone.liveAloneCommunity.controller.message;
 
+import com.capstone.liveAloneCommunity.dto.message.WriteMessageRequestDto;
 import com.capstone.liveAloneCommunity.entity.member.Member;
 import com.capstone.liveAloneCommunity.exception.member.MemberNotFoundException;
 import com.capstone.liveAloneCommunity.repository.member.MemberRepository;
+import com.capstone.liveAloneCommunity.response.Response;
+import com.capstone.liveAloneCommunity.service.message.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +20,16 @@ import org.springframework.web.bind.annotation.*;
 public class MessageController {
 
     private final MemberRepository memberRepository;
+    private final MessageService messageService;
+
+    @PostMapping("")
+    @Operation(summary = "쪽지 작성", description = "쪽지를 작성한다.")
+    @ResponseStatus(HttpStatus.OK)
+    public Response writeMessage(@RequestBody @Valid WriteMessageRequestDto writeMessageRequestDto) {
+        Member member = getMember();
+        Member receiver = getReceiver(writeMessageRequestDto.getReceiver());
+        return Response.success(messageService.writeMessage(member, receiver, writeMessageRequestDto));
+    }
 
     private Member getMember() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
