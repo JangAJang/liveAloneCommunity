f1215121d986f03cb710d7186e8b816fccb080df
eom-tae-in <eti0728@daum.net>
2023-05-20 04:49:55 +0900

    [feat] 검색 조건들을 정의할 enum 추상 클래스 추가

---
+++ src/main/java/com/capstone/liveAloneCommunity/repository/message/SearchMessageType.java
@@ -0,0 +1,19 @@
+package com.capstone.liveAloneCommunity.repository.message;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum SearchMessageType {
+
+    NAME, CONTENT, YEAR, MONTH, DATE, NOT;
+
+    @JsonValue
+    public String getName(){
+        return name();
+    }
+
+    @JsonCreator
+    public static SearchMessageType fromJson(String name){
+        return valueOf(name);
+    }
+}
