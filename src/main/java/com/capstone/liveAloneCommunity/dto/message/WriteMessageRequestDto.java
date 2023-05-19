b5ab92724029d27a7cb38d906f66e39b1fc9c797
eom-tae-in <eti0728@daum.net>
2023-05-19 23:18:12 +0900

    [feat] 회원이 작성한 메세지 정보를 담는 dto 생성

---
+++ src/main/java/com/capstone/liveAloneCommunity/dto/message/WriteMessageRequestDto.java
@@ -0,0 +1,21 @@
+package com.capstone.liveAloneCommunity.dto.message;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class WriteMessageRequestDto {
+
+    @NotBlank()
+    private String content;
+    @NotBlank
+    private String receiver;
+    @NotBlank
+    private String sender;
+}
