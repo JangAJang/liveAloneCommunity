956fa3f5cac74f57ed021899941b8af0dd04d7f8
eom-tae-in <eti0728@daum.net>
2023-05-19 23:13:28 +0900

    [feat] messageService 생성

---
+++ src/main/java/com/capstone/liveAloneCommunity/service/message/MessageService.java
@@ -0,0 +1,10 @@
+package com.capstone.liveAloneCommunity.service.message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MessageService {
+}
