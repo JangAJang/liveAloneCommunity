859b3eb8c4bcd203028ea40231cdb7b2ecb3bc9f
eom-tae-in <eti0728@daum.net>
2023-05-19 23:14:50 +0900

    [feat] messageService 작성 기능 구현

--- src/main/java/com/capstone/liveAloneCommunity/service/message/MessageService.java
+++ src/main/java/com/capstone/liveAloneCommunity/service/message/MessageService.java
@@ -1,4 +1,10 @@
 package com.capstone.liveAloneCommunity.service.message;
+
+import com.capstone.liveAloneCommunity.dto.message.MessageResponseDto;
+import com.capstone.liveAloneCommunity.dto.message.WriteMessageRequestDto;
+import com.capstone.liveAloneCommunity.entity.member.Member;
+import com.capstone.liveAloneCommunity.entity.message.Message;
+import com.capstone.liveAloneCommunity.repository.message.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,4 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+    public MessageResponseDto writeMessage(Member member, Member receiver, WriteMessageRequestDto writeMessageRequestDto) {
+        Message message = new Message(member, receiver, writeMessageRequestDto.getContent());
+        messageRepository.save(message);
+        return MessageResponseDto.toDto(message);
+    }
 }
