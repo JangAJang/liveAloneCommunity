0e38091142945922591687fd4337421b06920e82
eom-tae-in <eti0728@daum.net>
2023-05-19 23:05:35 +0900

    [feat] 현재 요청을 보낸 회원을 불러오는 메서드 생성

--- src/main/java/com/capstone/liveAloneCommunity/controller/message/MessageController.java
+++ src/main/java/com/capstone/liveAloneCommunity/controller/message/MessageController.java
@@ -1,6 +1,11 @@
 package com.capstone.liveAloneCommunity.controller.message;
 
+import com.capstone.liveAloneCommunity.entity.member.Member;
+import com.capstone.liveAloneCommunity.exception.member.MemberNotFoundException;
+import com.capstone.liveAloneCommunity.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,4 +13,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/message")
 public class MessageController {
 
+    private final MemberRepository memberRepository;
+
+    private Member getMember() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return memberRepository.findByUsername_Username(authentication.getName())
+                .orElseThrow(MemberNotFoundException::new);
+    }
 }
