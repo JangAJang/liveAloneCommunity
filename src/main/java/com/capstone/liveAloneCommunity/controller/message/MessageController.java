260ff09c601ce96d04f4c66b8c67fc3ee95705dc
eom-tae-in <eti0728@daum.net>
2023-05-19 23:01:21 +0900

    [feat] messageController 생성

---
+++ src/main/java/com/capstone/liveAloneCommunity/controller/message/MessageController.java
@@ -0,0 +1,11 @@
+package com.capstone.liveAloneCommunity.controller.message;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/message")
+public class MessageController {
+
+}
