49561af9acf1642b40c06cd0ed08fe5ec1be7a5d
eom-tae-in <eti0728@daum.net>
2023-05-20 04:32:04 +0900

    [feat] 검색 조건을 체크하는 메서드 생성

--- src/main/java/com/capstone/liveAloneCommunity/repository/message/MessageRepositoryCustomImpl.java
+++ src/main/java/com/capstone/liveAloneCommunity/repository/message/MessageRepositoryCustomImpl.java
@@ -1,4 +1,33 @@
 package com.capstone.liveAloneCommunity.repository.message;
 
+import com.capstone.liveAloneCommunity.dto.message.MessageResponseDto;
+import com.capstone.liveAloneCommunity.dto.message.MessageSearchRequestDto;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+
+@RequiredArgsConstructor
 public class MessageRepositoryCustomImpl implements MessageRepositoryCustom{
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Page<MessageResponseDto> searchMessage(MessageSearchRequestDto messageSearchRequestDto) {
+        return null;
+    }
+
+    private BooleanExpression checkCondition(MessageSearchRequestDto messageSearchRequestDto,
+                                             SearchMessageType searchMessageType, ReadMessageType readMessageType) {
+        if (searchMessageType.equals(SearchMessageType.NAME)) {
+            return ReadByNameCondition(messageSearchRequestDto, readMessageType);
+        }
+        if (searchMessageType.equals(SearchMessageType.CONTENT)) {
+            return ReadByContentCondition(messageSearchRequestDto)
+                    .and(checkReadCondition(readMessageType, messageSearchRequestDto));
+        }
+        if (searchMessageType.equals(SearchMessageType.NOT)) {
+            return checkReadCondition(readMessageType, messageSearchRequestDto);
+        }
+        return ReadByCalenderCondition(messageSearchRequestDto, searchMessageType, readMessageType);
+    }
 }
