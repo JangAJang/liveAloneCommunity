68a585da7c951169f19865c2e2498d0e1e5deef1
eom-tae-in <eti0728@daum.net>
2023-05-20 04:59:27 +0900

    [refactor] @QueryProjection를 생성자에 사용하기 위해 기존에 @AllArgsConstructor를 제거하고 직접 모든 변수를 매개변수로 가지는 생성자를 생성

--- src/main/java/com/capstone/liveAloneCommunity/dto/message/MessageResponseDto.java
+++ src/main/java/com/capstone/liveAloneCommunity/dto/message/MessageResponseDto.java
@@ -1,11 +1,10 @@
 package com.capstone.liveAloneCommunity.dto.message;
 
 import com.capstone.liveAloneCommunity.entity.message.Message;
-import lombok.AllArgsConstructor;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
 @Getter
 public class MessageResponseDto {
 
@@ -13,6 +12,14 @@ public class MessageResponseDto {
     private String receiver;
     private String sender;
     private LocalDateTime createDate;
+
+    @QueryProjection
+    public MessageResponseDto(String content, String receiver, String sender, LocalDateTime createDate) {
+        this.content = content;
+        this.receiver = receiver;
+        this.sender = sender;
+        this.createDate = createDate;
+    }
 
     public static MessageResponseDto toDto(Message message) {
         return new MessageResponseDto(
