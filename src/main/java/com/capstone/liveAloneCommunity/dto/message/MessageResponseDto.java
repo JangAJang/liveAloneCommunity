01e6175274e3ca342a0fe56ae1cfa43fde8f2fe9
eom-tae-in <eti0728@daum.net>
2023-05-19 23:27:58 +0900

    [feat] 응답으로 보낼 메세지 정보를 담는 responseDto 생성

---
+++ src/main/java/com/capstone/liveAloneCommunity/dto/message/MessageResponseDto.java
@@ -0,0 +1,28 @@
+package com.capstone.liveAloneCommunity.dto.message;
+
+import com.capstone.liveAloneCommunity.entity.message.Message;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class MessageResponseDto {
+
+    private String content;
+
+    private String receiver;
+
+    private String sender;
+
+    private LocalDateTime createDate;
+
+    public static MessageResponseDto toDto(Message message) {
+        return new MessageResponseDto(
+                message.getContent(),
+                message.getReceiverNickname(),
+                message.getSenderNickname(),
+                message.getCreatedDate());
+    }
+}
