9231aeece6d14c03e9cc471364784c47efcd0a78
eom-tae-in <eti0728@daum.net>
2023-05-20 18:02:00 +0900

    [feat] 송신자와 수신자가 같은 경우를 검증하는 메서드 추가

--- src/main/java/com/capstone/liveAloneCommunity/service/message/MessageService.java
+++ src/main/java/com/capstone/liveAloneCommunity/service/message/MessageService.java
@@ -4,6 +4,7 @@ import com.capstone.liveAloneCommunity.dto.message.MessageResponseDto;
 import com.capstone.liveAloneCommunity.dto.message.WriteMessageRequestDto;
 import com.capstone.liveAloneCommunity.entity.member.Member;
 import com.capstone.liveAloneCommunity.entity.message.Message;
+import com.capstone.liveAloneCommunity.exception.message.CanNotSameReceiverAndSenderException;
 import com.capstone.liveAloneCommunity.repository.message.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,8 +18,15 @@ public class MessageService {
     private final MessageRepository messageRepository;
 
     public MessageResponseDto writeMessage(Member member, Member receiver, WriteMessageRequestDto writeMessageRequestDto) {
+        isSameSenderAndReceiver(member, receiver);
         Message message = new Message(member, receiver, writeMessageRequestDto.getContent());
         messageRepository.save(message);
         return MessageResponseDto.toDto(message);
+    }
+
+    private static void isSameSenderAndReceiver(Member member, Member receiver) {
+        if (member.equals(receiver)) {
+            throw new CanNotSameReceiverAndSenderException();
+        }
     }
 }
