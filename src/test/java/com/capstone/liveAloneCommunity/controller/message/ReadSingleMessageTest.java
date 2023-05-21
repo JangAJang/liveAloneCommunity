ed229c5b65fcc4bf9821983b97b4421e557dcf2a
eom-tae-in <eti0728@daum.net>
2023-05-21 19:12:25 +0900

    [test] 회원의 accessTocken을 반환해주는 메서드 생성

--- src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadSingleMessageTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadSingleMessageTest.java
@@ -1,9 +1,22 @@
 package com.capstone.liveAloneCommunity.controller.message;
 
+import com.capstone.liveAloneCommunity.dto.auth.LogInRequestDto;
+import com.capstone.liveAloneCommunity.service.auth.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ReadSingleMessageTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private AuthService authService;
+
+    private String getAccessTokenAfterLogIn(String text){
+        return authService.logIn(LogInRequestDto.builder().username(text).password(text).build()).getAccessToken();
+    }
 }
