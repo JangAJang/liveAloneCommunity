c83052c3c85a81f300282fbcbb6e9cbda1557c37
eom-tae-in <eti0728@daum.net>
2023-05-21 15:23:42 +0900

    [test] 메세지 작성 기능을 테스트하는 테스트 클래스 생성

---
+++ src/test/java/com/capstone/liveAloneCommunity/controller/message/WriteMessageTest.java
@@ -0,0 +1,24 @@
+package com.capstone.liveAloneCommunity.controller.message;
+
+import com.capstone.liveAloneCommunity.DatabaseCleanup;
+import com.capstone.liveAloneCommunity.dto.auth.RegisterRequestDto;
+import com.capstone.liveAloneCommunity.service.auth.AuthService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class WriteMessageTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private AuthService authService;
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+}
+
