1731e746072f268ca5cf09600b2b8550542901a2
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-05-22 00:27:40 +0900

    [feat] 댓글에 대한 페이지 전환 로직 구현

--- src/main/java/com/capstone/liveAloneCommunity/dto/comment/ReadCommentResponseDto.java
+++ src/main/java/com/capstone/liveAloneCommunity/dto/comment/ReadCommentResponseDto.java
@@ -3,17 +3,26 @@ package com.capstone.liveAloneCommunity.dto.comment;
 import com.capstone.liveAloneCommunity.entity.comment.Comment;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Builder
 @Getter
 public class ReadCommentResponseDto {
 
-    private String title;
+    private String createdDate;
+    private String nickname;
     private String content;
 
     public static ReadCommentResponseDto toDto(Comment comment) {
         return ReadCommentResponseDto.builder()
-                .title(comment.getPostTitle())
                 .content(comment.getContent())
+                .nickname(comment.getWriterName())
+                .createdDate(getCreatedDateToString(comment.getCreatedDate()))
                 .build();
+    }
+
+    private static String getCreatedDateToString(LocalDateTime createdDate){
+        return createdDate.getYear() + ". " +createdDate.getMonthValue() + ". " + createdDate.getDayOfMonth()
+                + ". " + createdDate.getHour() + ":" + createdDate.getMinute();
     }
 }
