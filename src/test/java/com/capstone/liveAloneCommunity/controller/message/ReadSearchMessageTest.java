2d1599b0d33abb7c8598c8481bf353c9bd454959
eom-tae-in <eti0728@daum.net>
2023-05-22 08:44:19 +0900

    [test] 쪽지 검색 조회 기능을 테스트하는 테스트 클래스 생성

---
+++ src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadSearchMessageTest.java
@@ -0,0 +1,15 @@
+package com.capstone.liveAloneCommunity.controller.message;
+
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ReadSearchMessageTest {
+
+    @Autowired
+    private MockMvc mvc;
+}
