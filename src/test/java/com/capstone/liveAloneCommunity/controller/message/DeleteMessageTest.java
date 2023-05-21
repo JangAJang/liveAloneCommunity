a4a0eaf34236aaeb740a7facbf2ba7317f5b89df
eom-tae-in <eti0728@daum.net>
2023-05-21 22:09:29 +0900

    [test] 회원을 등록하는 메서드 생성

---
+++ src/test/java/com/capstone/liveAloneCommunity/controller/message/DeleteMessageTest.java
@@ -0,0 +1,31 @@
+package com.capstone.liveAloneCommunity.controller.message;
+
+import com.capstone.liveAloneCommunity.dto.auth.LogInRequestDto;
+import com.capstone.liveAloneCommunity.dto.auth.RegisterRequestDto;
+import com.capstone.liveAloneCommunity.service.auth.AuthService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DeleteMessageTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private AuthService authService;
+
+    private void registerMember(String text) {
+        authService.register(RegisterRequestDto.builder()
+                .username(text)
+                .nickname(text)
+                .email(text + "@" + text + ".com")
+                .password(text)
+                .passwordCheck(text)
+                .build());
+    }
+}
+
