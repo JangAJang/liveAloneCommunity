13c2df3dbef74b34ffbc0d6de5aaa66efa3f9e8f
eom-tae-in <eti0728@daum.net>
2023-05-21 19:14:51 +0900

    [test] 쪽지를 작성하는 메서드 생성

--- src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadSingleMessageTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadSingleMessageTest.java
@@ -4,10 +4,13 @@ import com.capstone.liveAloneCommunity.dto.auth.LogInRequestDto;
 import com.capstone.liveAloneCommunity.entity.member.Member;
 import com.capstone.liveAloneCommunity.repository.member.MemberRepository;
 import com.capstone.liveAloneCommunity.service.auth.AuthService;
+import com.capstone.liveAloneCommunity.service.message.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,6 +22,14 @@ public class ReadSingleMessageTest {
     private AuthService authService;
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private MessageService messageService;
+
+    private void sendMessage(Member sender, Member receiver, int count) {
+        IntStream.range(0, count).forEach(i -> {
+            messageService.writeMessage(sender, receiver, "message" + i);
+        });
+    }
 
     private String getAccessTokenAfterLogIn(String text){
         return authService.logIn(LogInRequestDto.builder().username(text).password(text).build()).getAccessToken();
