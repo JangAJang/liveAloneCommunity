abccd2be0bac0c49ddff99a7c2458cb8180211ac
eom-tae-in <eti0728@daum.net>
2023-05-22 07:07:37 +0900

    [test] 회원을 등록하는 메서드 생성

---
+++ src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadReceiverMessageTest.java
@@ -0,0 +1,27 @@
+package com.capstone.liveAloneCommunity.controller.message;
+
+import com.capstone.liveAloneCommunity.dto.auth.RegisterRequestDto;
+import com.capstone.liveAloneCommunity.service.auth.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ReadReceiverMessageTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private AuthService authService;
+
+    private void registerMember(String text) {
+        authService.register(RegisterRequestDto.builder()
+                .username(text)
+                .nickname(text)
+                .email(text + "@" + text + ".com")
+                .password(text)
+                .passwordCheck(text)
+                .build());
+    }
+}
