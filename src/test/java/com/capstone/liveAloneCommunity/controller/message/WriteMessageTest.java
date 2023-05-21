3ee752c6feb597a94147ca29d3851d17af34d005
eom-tae-in <eti0728@daum.net>
2023-05-21 15:32:35 +0900

    [test] 회원 가입하는 로직 메서드로 분리

--- src/test/java/com/capstone/liveAloneCommunity/controller/message/WriteMessageTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/controller/message/WriteMessageTest.java
@@ -5,6 +5,8 @@ import com.capstone.liveAloneCommunity.dto.auth.RegisterRequestDto;
 import com.capstone.liveAloneCommunity.service.auth.AuthService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,18 +25,22 @@ public class WriteMessageTest {
 
     @BeforeEach
     void initData() {
-        authService.register(RegisterRequestDto.builder()
-                .username("test")
-                .nickname("test")
-                .email("test@test.com")
-                .password("test")
-                .passwordCheck("test")
-                .build());
+        registerMember("test");
     }
 
     @AfterEach
     void clearDB() {
         databaseCleanup.execute();
+    }
+
+    private void registerMember(String text) {
+        authService.register(RegisterRequestDto.builder()
+                .username(text)
+                .nickname(text)
+                .email(text + "@" + text + ".com")
+                .password(text)
+                .passwordCheck(text)
+                .build());
     }
 }
 
