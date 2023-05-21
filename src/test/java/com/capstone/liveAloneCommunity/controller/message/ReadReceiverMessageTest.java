aa04284ec17d88810760130267c0403d8a3e7883
eom-tae-in <eti0728@daum.net>
2023-05-22 07:08:40 +0900

    [test] 쪽지를 생성하는 메서드 생성

--- src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadReceiverMessageTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadReceiverMessageTest.java
@@ -1,11 +1,16 @@
 package com.capstone.liveAloneCommunity.controller.message;
 
 import com.capstone.liveAloneCommunity.dto.auth.RegisterRequestDto;
+import com.capstone.liveAloneCommunity.entity.member.Member;
 import com.capstone.liveAloneCommunity.service.auth.AuthService;
+import com.capstone.liveAloneCommunity.service.message.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.stream.IntStream;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ReadReceiverMessageTest {
@@ -14,6 +19,8 @@ public class ReadReceiverMessageTest {
     private MockMvc mvc;
     @Autowired
     private AuthService authService;
+    @Autowired
+    private MessageService messageService;
 
     private void registerMember(String text) {
         authService.register(RegisterRequestDto.builder()
@@ -23,5 +30,11 @@ public class ReadReceiverMessageTest {
                 .password(text)
                 .passwordCheck(text)
                 .build());
+    }
+
+    private void sendMessage(Member sender, Member receiver, String text, int count) {
+        IntStream.range(0, count).forEach(i -> {
+            messageService.writeMessage(sender, receiver, text + i);
+        });
     }
 }
