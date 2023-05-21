f3f1e5110b97546cf0e3157b4ed8f74fa9e74aac
eom-tae-in <eti0728@daum.net>
2023-05-22 07:10:28 +0900

    [test] 회원의 accessToken을 반환하는 메서드 생성

--- src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadReceiverMessageTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadReceiverMessageTest.java
@@ -1,5 +1,6 @@
 package com.capstone.liveAloneCommunity.controller.message;
 
+import com.capstone.liveAloneCommunity.dto.auth.LogInRequestDto;
 import com.capstone.liveAloneCommunity.dto.auth.RegisterRequestDto;
 import com.capstone.liveAloneCommunity.entity.member.Member;
 import com.capstone.liveAloneCommunity.service.auth.AuthService;
@@ -41,4 +42,9 @@ public class ReadReceiverMessageTest {
     private void deleteMessage(Member member, Long id) {
         messageService.deleteMessage(member, id);
     }
+
+    private String getAccessTokenAfterLogIn(String text){
+        return authService.logIn(LogInRequestDto.builder().username(text).password(text).build()).getAccessToken();
+    }
+
 }
