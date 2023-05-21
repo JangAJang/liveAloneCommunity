76ac44e111f794e0c912e3bb8bc83a221227283f
eom-tae-in <eti0728@daum.net>
2023-05-22 07:11:54 +0900

    [test] 회원의 닉네임으로 회원 정보를 찾아 반환하는 메서드 생성

--- src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadReceiverMessageTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadReceiverMessageTest.java
@@ -3,6 +3,7 @@ package com.capstone.liveAloneCommunity.controller.message;
 import com.capstone.liveAloneCommunity.dto.auth.LogInRequestDto;
 import com.capstone.liveAloneCommunity.dto.auth.RegisterRequestDto;
 import com.capstone.liveAloneCommunity.entity.member.Member;
+import com.capstone.liveAloneCommunity.repository.member.MemberRepository;
 import com.capstone.liveAloneCommunity.service.auth.AuthService;
 import com.capstone.liveAloneCommunity.service.message.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,8 @@ public class ReadReceiverMessageTest {
     private AuthService authService;
     @Autowired
     private MessageService messageService;
+    @Autowired
+    private MemberRepository memberRepository;
 
     private void registerMember(String text) {
         authService.register(RegisterRequestDto.builder()
@@ -47,4 +50,7 @@ public class ReadReceiverMessageTest {
         return authService.logIn(LogInRequestDto.builder().username(text).password(text).build()).getAccessToken();
     }
 
+    private Member getMember(String nickname) {
+        return memberRepository.findByNickname_Nickname(nickname).orElseThrow(MemberNotFoundException::new);
+    }
 }
