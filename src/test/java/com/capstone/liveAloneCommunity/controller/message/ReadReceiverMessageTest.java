43101576c8a7a31dc9bc0356881abb26143dae0e
eom-tae-in <eti0728@daum.net>
2023-05-22 07:09:11 +0900

    [test] 쪽지를 삭제하는 메서드 생성

--- src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadReceiverMessageTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/controller/message/ReadReceiverMessageTest.java
@@ -37,4 +37,8 @@ public class ReadReceiverMessageTest {
             messageService.writeMessage(sender, receiver, text + i);
         });
     }
+
+    private void deleteMessage(Member member, Long id) {
+        messageService.deleteMessage(member, id);
+    }
 }
