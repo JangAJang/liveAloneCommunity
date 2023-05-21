d733318638efaae2b0dc5c18287bbbf9b77907dc
eom-tae-in <eti0728@daum.net>
2023-05-21 22:12:09 +0900

    [test] 쪽지를 생성하는 메서드 생성

--- src/test/java/com/capstone/liveAloneCommunity/controller/message/DeleteMessageTest.java
+++ src/test/java/com/capstone/liveAloneCommunity/controller/message/DeleteMessageTest.java
@@ -2,12 +2,16 @@ package com.capstone.liveAloneCommunity.controller.message;
 
 import com.capstone.liveAloneCommunity.dto.auth.LogInRequestDto;
 import com.capstone.liveAloneCommunity.dto.auth.RegisterRequestDto;
+import com.capstone.liveAloneCommunity.entity.member.Member;
 import com.capstone.liveAloneCommunity.service.auth.AuthService;
+import com.capstone.liveAloneCommunity.service.message.MessageService;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,6 +21,9 @@ public class DeleteMessageTest {
     private MockMvc mvc;
     @Autowired
     private AuthService authService;
+    @Autowired
+    private MessageService messageService;
+
 
     private void registerMember(String text) {
         authService.register(RegisterRequestDto.builder()
@@ -26,6 +33,12 @@ public class DeleteMessageTest {
                 .password(text)
                 .passwordCheck(text)
                 .build());
+    }
+
+    private void sendMessage(Member sender, Member receiver, int count) {
+        IntStream.range(0, count).forEach(i -> {
+            messageService.writeMessage(sender, receiver, "message" + i);
+        });
     }
 
     private String getAccessTokenAfterLogIn(String text){
