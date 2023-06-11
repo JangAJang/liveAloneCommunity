cdee94a95be6cd6cc9ccab1fc8b314e173f84bdd
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-06-11 23:25:41 +0900

    [feat] 게시물을 작성할 때 작성하는 회원의 위치를 저장하게 기능 구현

--- src/main/java/com/capstone/liveAloneCommunity/entity/post/Post.java
+++ src/main/java/com/capstone/liveAloneCommunity/entity/post/Post.java
@@ -6,12 +6,10 @@ import com.capstone.liveAloneCommunity.domain.post.Title;
 import com.capstone.liveAloneCommunity.entity.BaseTimeEntity;
 import com.capstone.liveAloneCommunity.entity.member.Member;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.locationtech.jts.geom.Point;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -36,12 +34,17 @@ public class Post extends BaseTimeEntity {
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Member member;
 
+    @Column
+    private Point point;
+
     @Builder
+    @SneakyThrows
     public Post(Title title, Content content, Member member, Category category){
         this.title = title;
         this.content = content;
         this.member = member;
         this.category = category;
+        this.point = member.getPoint();
     }
 
     public String getTitle(){
